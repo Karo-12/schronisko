fda96d72a6cc394a432b80efc48e25a021f1e1ba
Karolina <200626@student.pwr.edu.pl>
2021-11-24 12:25:51 +0100

    Exchanging files

--- src/main/java/pl/schronisko/repository/AnimalRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.schronisko.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import pl.schronisko.model.Animal;
-import pl.schronisko.model.AnimalId;
-
-public interface AnimalRepository extends CrudRepository<Animal, AnimalId> {
-}
--- src/main/java/pl/schronisko/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.schronisko.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import pl.schronisko.model.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
